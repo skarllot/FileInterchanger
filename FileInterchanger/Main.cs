29c8372645863186981878caeb3d2e8d43cf8bd8
Fabrício Godoy <fgodoy@rodrimar.com.br>
2013-11-08 13:54:37 -0200

    Added license information

--- FileInterchanger/Main.cs
+++ FileInterchanger/Main.cs
@@ -1,4 +1,22 @@
-﻿using System.ServiceProcess;
+﻿// Main.cs
+//
+// Copyright (C) 2013 Fabrício Godoy
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System.ServiceProcess;
 
 namespace FileInterchanger
 {
@@ -9,7 +27,7 @@ namespace FileInterchanger
         public const string PROGRAM_NAME = "FileInterchanger";
         // Latest release: 
         // Major.Minor.Maintenance.Revision
-        public const string PROGRAM_VERSION = "0.1.0.43";
+        public const string PROGRAM_VERSION = "0.1.0.44";
         public const string PROGRAM_TITLE = PROGRAM_NAME + " 0.1.0";
 
         public static void Main(string[] args)
---
+++ README.txt
@@ -0,0 +1,6 @@
+FileInterchanger by Fabrício Godoy <skarllot@gmail.com>
+
+FileInterchanger is a service to interchange files between local or shared
+directory and FTPthat uses WinSCP as backend.
+
+https://github.com/skarllot/FileInterchanger
